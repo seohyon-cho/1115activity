5135bda50365ca616cc0c81d0d270611ebd1959a
seohyon-cho <flanique@naver.com>
2023-11-15 12:42:41 +0900

    시간대별 theme 변경 함수 추가

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const screen = document.querySelector('.screen');
 const numbers = screen.querySelectorAll('span');
 const ampm = document.querySelector('em.on');
+const main = document.querySelector('main');
 
 setInterval(() => {
 	ampm.innerText = new Date().getHours() < 12 ? 'am' : 'pm';
@@ -8,6 +9,7 @@ setInterval(() => {
 	// 반환된 배열값을 그대로 반복 돌면서 각각 setTime 함수에 인수로 전달됨
 	// setTime 함수는 반복을 돌면서 시간, 분, 초가 한 자릿수일 때 0을 붙여주는 공통의 로직을 반복 실행함.
 	getTime().forEach((num, idx) => setTime(num, idx));
+	changeTheme();
 }, 1000);
 
 // 시간값을 구해서 반환하는 함수
@@ -24,4 +26,22 @@ function getTime() {
 // 반환된 시간값을 인수로 받아서 DOM에 셋팅하는 함수
 function setTime(num, index) {
 	numbers[index].innerText = num < 10 ? '0' + num : num;
+}
+
+function changeTheme() {
+	const hr = new Date().getHours();
+	main.className = '';
+
+	if (hr >= 5 && hr < 11) {
+		main.classList.add('morning');
+	}
+	if (hr >= 11 && hr < 16) {
+		main.classList.add('afternoon');
+	}
+	if (hr >= 16 && hr < 20) {
+		main.classList.add('evening');
+	}
+	if (hr >= 20 || hr < 5) {
+		main.classList.add('night');
+	}
 }
