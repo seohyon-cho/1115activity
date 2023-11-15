03d187aeddf008766ccb914b5b8836f7b91bbaaf
seohyon-cho <flanique@naver.com>
2023-11-15 13:16:25 +0900

    자주 바뀔만한 값을 상단에 전역변수로 뺀 뒤, 해당 값을 필요로 하는 함수에 인수로 전달 (데이터 추적을 편하게 하기 위해서)

--- js/main.js
+++ js/main.js
@@ -2,7 +2,15 @@ const screen = document.querySelector('.screen');
 const numbers = screen.querySelectorAll('span');
 const ampm = document.querySelector('em.on');
 const main = document.querySelector('main');
-const hr = new Date().getHours();
+
+// 자주 바뀔만한 값을 전역변수 형태로, 객체를 배열로 묶어두는 형태로 따로 빼서 관리하는 것.
+// 해당 값이 아래 함수에서 호출되도록 처리
+const data = [
+	{ time: new Date().getHours() >= 5 && new Date().getHours() < 11, name: 'morning' },
+	{ time: new Date().getHours() >= 11 && new Date().getHours() < 16, name: 'afternoon' },
+	{ time: new Date().getHours() >= 16 && new Date().getHours() < 20, name: 'evening' },
+	{ time: new Date().getHours() >= 20 || new Date().getHours() < 5, name: 'night' },
+];
 
 setInterval(() => {
 	ampm.innerText = new Date().getHours() < 12 ? 'am' : 'pm';
@@ -10,7 +18,8 @@ setInterval(() => {
 	// 반환된 배열값을 그대로 반복 돌면서 각각 setTime 함수에 인수로 전달됨
 	// setTime 함수는 반복을 돌면서 시간, 분, 초가 한 자릿수일 때 0을 붙여주는 공통의 로직을 반복 실행함.
 	getTime().forEach((num, idx) => setTime(num, idx));
-	changeTheme();
+	// 전역변수인 data를 인수로 받아서 호출 처리
+	changeTheme(data);
 }, 1000);
 
 // 시간값을 구해서 반환하는 함수
@@ -29,11 +38,12 @@ function setTime(num, index) {
 	numbers[index].innerText = num < 10 ? '0' + num : num;
 }
 
-function changeTheme() {
-	const hr = new Date().getHours();
+function changeTheme(info) {
+	// const hr = new Date().getHours();
+	// 전역 data 값을 바로 활용하는 것이 아닌, info라는 파라미터를 통해서 전달받도록 처리하는 것.
 	main.className = '';
 
-	if (hr >= 5 && hr < 11) {
+	/*if (hr >= 5 && hr < 11) {
 		main.classList.add('morning');
 	}
 	if (hr >= 11 && hr < 16) {
@@ -44,16 +54,9 @@ function changeTheme() {
 	}
 	if (hr >= 20 || hr < 5) {
 		main.classList.add('night');
-	}
+	}*/
+
+	info.forEach((el) => {
+		if (el.time) main.classList.add(el.name);
+	});
 }
-
-const data = [
-	{ time: hr >= 5 && hr < 11, name: 'morning' },
-	{ time: hr >= 11 && hr < 16, name: 'afternoon' },
-	{ time: hr >= 16 && hr < 20, name: 'evening' },
-	{ time: hr >= 20 || hr < 5, name: 'night' },
-];
-
-data.forEach((el) => {
-	if (el.time) main.classList.add(el.name);
-});
