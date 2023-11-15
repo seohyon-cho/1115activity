f3f1b5eb28f1447fdb88725caf90856613291c2e
seohyon-cho <flanique@naver.com>
2023-11-15 12:02:43 +0900

    현재 시 값이 13이상이 되면, 12씩 빼서 13시가 아닌 01시로 출력

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 const screen = document.querySelector('.screen');
-const [spanHr, spanMin, spanSec] = screen.querySelectorAll('span');
+const numbers = screen.querySelectorAll('span');
 const ampm = document.querySelector('em.on');
 
 setInterval(() => {
+	ampm.innerText = new Date().getHours() < 12 ? 'am' : 'pm';
+	// getTime 함수가 [hr, min, sec] 배열을 반환
+	// 반환된 배열값을 그대로 반복 돌면서 각각 setTime 함수에 인수로 전달됨
+	// setTime 함수는 반복을 돌면서 시간, 분, 초가 한 자릿수일 때 0을 붙여주는 공통의 로직을 반복 실행함.
+	getTime().forEach((num, idx) => setTime(num, idx));
+}, 1000);
+
+// 시간값을 구해서 반환하는 함수
+function getTime() {
 	const now = new Date();
-	let hr = now.getHours();
+	let hr = now.getHours() > 12 ? now.getHours() - 12 : now.getHours();
 	let min = now.getMinutes();
 	let sec = now.getSeconds();
+	// 현재 '시'값이 13이상이 되면, 12를 뺀 값을 hr로 반환
+	hr = hr > 12 ? hr - 12 : hr;
+	return [hr, min, sec];
+}
 
-	spanHr.innerText = hr < 10 ? '0' + hr : hr;
-	spanMin.innerText = min < 10 ? '0' + min : min;
-	spanSec.innerText = sec < 10 ? '0' + sec : sec;
-
-	ampm.innerText = hr < 12 ? 'am' : 'pm';
-}, 1000);
+// 반환된 시간값을 인수로 받아서 DOM에 셋팅하는 함수
+function setTime(num, index) {
+	numbers[index].innerText = num < 10 ? '0' + num : num;
+}
