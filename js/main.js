1fd04e4a8d96a48e2f36173db30c59b266285dc7
seohyon-cho <flanique@naver.com>
2023-11-16 09:53:43 +0900

    메뉴 버튼 클릭 시 강제 테마 변경 기능 종료 및 수동으로 테마 변경

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const screen = document.querySelector('.screen');
 const numbers = screen.querySelectorAll('span');
 const ampm = document.querySelector('em.on');
 const main = document.querySelector('main');
+const btns = document.querySelectorAll('nav span');
 
 // 자주 바뀔만한 값을 전역변수 형태로, 객체를 배열로 묶어두는 형태로 따로 빼서 관리하는 것.
 // 해당 값이 아래 함수에서 호출되도록 처리
@@ -12,7 +13,7 @@ const data = [
 	{ time: new Date().getHours() >= 20 || new Date().getHours() < 5, name: 'night' },
 ];
 
-setInterval(() => {
+let timer = setInterval(() => {
 	ampm.innerText = new Date().getHours() < 12 ? 'am' : 'pm';
 	// getTime 함수가 [hr, min, sec] 배열을 반환
 	// 반환된 배열값을 그대로 반복 돌면서 각각 setTime 함수에 인수로 전달됨
@@ -21,6 +22,22 @@ setInterval(() => {
 	// 전역변수인 data를 인수로 받아서 호출 처리
 	changeTheme(data);
 }, 1000);
+
+btns.forEach((btn) => {
+	// 각 버튼 클릭 시
+	btn.addEventListener('click', (e) => {
+		// 클릭한 버튼만 활성화 처리
+		btns.forEach((btn) => btn.classList.remove('on'));
+		e.currentTarget.classList.add('on');
+
+		// 기존의 자동 롤링 기능 끊어줌.
+		clearInterval(timer);
+		// main의 모든 클래스 제거
+		main.className = '';
+		// 클릭한 버튼의 글자를 가져와서 소문자로 변경한 다음, 그 내용을 main의 클래스명으로 지정.
+		main.classList.add(e.currentTarget.innerText.toLowerCase());
+	});
+});
 
 // 시간값을 구해서 반환하는 함수
 function getTime() {
